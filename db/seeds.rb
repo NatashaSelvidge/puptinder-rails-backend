54480df9d0965d473264e1e99885d84131946dcb
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-11 13:30:42 -0700

    created dogs table and seeded data

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+ 
+Dog.create(name:"Mako", status: "undecided", image_url: "https://images.pexels.com/photos/2304910/pexels-photo-2304910.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+Dog.create(name:"Ralph", status: "undecided", image_url: "https://images.pexels.com/photos/773971/pexels-photo-773971.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+Dog.create(name:"Russo", status: "undecided", image_url: "https://images.pexels.com/photos/434090/pexels-photo-434090.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+Dog.create(name:"Bailey", status: "undecided", image_url: "https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+Dog.create(name:"Jackson", status: "undecided", image_url: "https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+Dog.create(name:"Titan", status: "undecided", image_url: "https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
